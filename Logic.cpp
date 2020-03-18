dc82b33353659e39d6f00bf0b698b741cd9006bd
buyalova-uliana <buyalova@yandex.ru>
2020-03-18 19:00:17 +0300

    add moving blocks

--- Logic.cpp
+++ Logic.cpp
@@ -5,60 +5,128 @@
 Logic::Logic(): snake(snake_start_position) 
 {
      srand(time(0));
-     draw_a_field();
-     food = generate_food_position();
+	 block.resize(count_of_block);
+	 for (int i = 0; i < block.size(); ++i)
+		 block[i] = { 0, 0 };
+	 GenerateBlockPosition();
+     DrawTheField();
+     food = GenerateFoodPosition();
 }
 
-void Logic::draw_a_field()
+void Logic::DrawTheField()
 {
     
-    for(short i = 0; i < weight; ++i)
+    for(short i = 0; i < width; ++i)
         pr.Print(Point{ i, 0 }, '-');
-    for (short i = 0; i < weight; ++i)
-        pr.Print(Point{ i, hight }, '-');
-    for (short i = 0; i < hight; ++i)
+    for (short i = 0; i < width; ++i)
+        pr.Print(Point{ i, height }, '-');
+    for (short i = 0; i < height; ++i)
         pr.Print(Point{ 0, i }, '|');
-    for (short i = 0; i < hight; ++i)
-        pr.Print(Point{ weight, i }, '|');
+    for (short i = 0; i < height; ++i)
+        pr.Print(Point{ width , i }, '|');
+	for (int i = 0; i < count_of_block; ++i)
+		pr.Print(block[i], block_symbol);
 }
 
-Point Logic::change(Point p, char c)
+Point Logic::Change(Point p, char c)
 {
     if (c == 'a')
     {
-        p.x -= (p.x == 1) ? 0 : 1;
+		bool a = PointBelongsToTheBlock({ p.x - 1, p.y });
+		if (a)
+		{
+			int h = 0;
+		}
+		p.x -= ((p.x == 1) || (PointBelongsToTheBlock({p.x - 1, p.y}))) ? 0 : 1;
     }
     if (c == 'w')
     {
-        p.y -= (p.y == 1) ? 0 : 1;
+		bool b = PointBelongsToTheBlock({ p.x, p.y - 1});
+		if (b)
+		{
+			int h = 0;
+		}
+        p.y -= ((p.y == 1) || (PointBelongsToTheBlock({ p.x, p.y - 1 }))) ? 0 : 1;
     }
     if (c == 'd')
     {
-        p.x += (p.x == (weight - 1)) ? 0 : 1; 
+		bool c = PointBelongsToTheBlock({ p.x + 1, p.y });
+		if (c)
+		{
+			int h = 0;
+		}
+        p.x += ((p.x == (width - 1)) || (PointBelongsToTheBlock({ p.x + 1, p.y})))? 0 : 1;
     }
-    if (c == 's')
-        p.y += (p.y == (hight - 1)) ? 0 : 1;
+	if (c == 's')
+	{
+		bool d = PointBelongsToTheBlock({ p.x, p.y + 1 });
+		if (d)
+		{
+			int h = 0;
+		}
+		p.y += ((p.y == (height - 1)) || (PointBelongsToTheBlock({ p.x, p.y + 1 }))) ? 0 : 1;
+	}
     return p;
 }
 
-Point Logic::generate_food_position()
+Point Logic::GeneratePoint()
 {
-    Point p;
-    do
-    {
-        p.x = rand() % (weight - 1) + 1;
-        p.y = rand() % (hight - 1) + 1;
-    } while (snake.PointBelongsToTheSnake(p));
-    
-    return p;
+	Point p;
+	do
+	{
+		p.x = rand() % (width - 1) + 1;
+		p.y = rand() % (height - 1) + 1;
+	} while (snake.PointBelongsToTheSnake(p) && PointBelongsToTheBlock(p));
+
+	return p;
 }
 
+Point Logic::GenerateFoodPosition()
+{
+   return GeneratePoint();
+}
 
-char Logic::generate_new_direction()
+bool Logic::PointBelongsToTheBlock(Point p)
+{
+	for (int i = 0; i < block.size(); ++i)
+	{
+		if (p == block[i])
+			return true;
+	}
+	return false;
+}
+
+void Logic::GenerateBlockPosition()
+{
+	for(int i = 0; i < count_of_block; ++i)
+	    block[i] = GeneratePoint();
+}
+
+char Logic::GenerateNewDirection()
 {
     std::vector<char> v{'a','w', 'd', 's'};
     int number = rand() % 4;
     return v[number];
+}
+
+void Logic::move_block(Point& p, int i)
+{
+	char dir;
+	Point new_p;
+	while (true)
+	{
+		dir = GenerateNewDirection();
+		new_p = Change(p, dir);
+		if (new_p == food)
+			continue;
+		else
+		{
+			pr.Clear(p);
+			p = new_p;
+			pr.Print(p, i + '0');
+			return;
+		}
+	}
 }
 
 void Logic::Run()
@@ -66,12 +134,12 @@ void Logic::Run()
     bool cond = true;
     bool you_win = false;
     char new_dir = 0; // новое направление, в котором движется змея
-    std::thread thr1(&Logic::threadFunction1, *this, std::ref(new_dir), std::ref(you_win));
-    std::thread thr2(&Logic::threadFunction2, *this, std::ref(new_dir), std::ref(cond));
+    std::thread thr1(&Logic::ThreadFunction1, *this, std::ref(new_dir), std::ref(you_win));
+    std::thread thr2(&Logic::ThreadFunction2, *this, std::ref(new_dir), std::ref(cond));
 
     thr1.join();
     cond = false;
-    clear();
+    Clear();
     if(you_win == false)
         pr.Print(game_over_point_position1, game_over1);
     else
@@ -80,15 +148,34 @@ void Logic::Run()
     thr2.join();
 }
 
-void Logic::threadFunction1(char& new_dir, bool& you_win)
+void Logic::ThreadFunction1(char& new_dir, bool& you_win)
 {
     pr.Print(food, food_symbol);
     pr.Print(snake.Head(), snake_symbol);
     Point new_pos;
+	int count = 1;
     while (true)
     {
-        new_pos = change(snake.Head(), new_dir);
-        std::this_thread::sleep_for(250ms);
+        new_pos = Change(snake.Head(), new_dir);
+        std::this_thread::sleep_for(200ms);
+		/////////////////////////////////
+		if (count % 3 == 0)
+		{
+			for (int i = 0; i < count_of_block; ++i)
+			{
+				move_block(block[i], i);
+				
+				for (int j = 1; j < i; ++j)
+				{
+					if (block[i] == block[j])
+						--i;
+				}
+				
+				//move_block(block[i]);
+			}
+		}
+		/////////////////////////////////
+		++count;
         if (new_pos == snake.Head())
             continue;
         if (snake.PointBelongsToTheSnake(new_pos))
@@ -98,6 +185,8 @@ void Logic::threadFunction1(char& new_dir, bool& you_win)
             else
                 return;
         }
+		
+
         if (food == new_pos)
         {
             snake.Add(new_pos);
@@ -106,7 +195,7 @@ void Logic::threadFunction1(char& new_dir, bool& you_win)
                 you_win = true;
                 return;
             }
-            food = generate_food_position();
+            food = GenerateFoodPosition();
             pr.Print(food, food_symbol);
         }
         else
@@ -117,11 +206,11 @@ void Logic::threadFunction1(char& new_dir, bool& you_win)
         }
 
         pr.Print(new_pos, snake_symbol);
-
+		
     }
 }
 
-void Logic::threadFunction2(char& new_dir, bool& cond)
+void Logic::ThreadFunction2(char& new_dir, bool& cond)
 {
     char c;
     while (cond)
@@ -134,13 +223,14 @@ void Logic::threadFunction2(char& new_dir, bool& cond)
     } 
 }
 
-void Logic::clear()
+void Logic::Clear()
 {
-    for (short i = 1; i < hight - 1; ++i)
+    for (short i = 1; i < height - 1; ++i)
     {
-        for (short j = 1; j < weight - 1; ++j)
+        for (short j = 1; j < width - 1; ++j)
         {
             pr.Clear({ j, i });
         }
     }
 }
+
--- Logic.h
+++ Logic.h
@@ -5,31 +5,38 @@
 
 class Logic
 {
-    const short hight = 25;
-    const short weight = 45;
+    const short height = 25;
+    const short width = 45;
     const char snake_symbol = 219;
     const char food_symbol = '*';
     const Point snake_start_position{ 10, 10 };
-    const Point game_over_point_position1{ weight / 2 - 5, hight / 2 };
+    const Point game_over_point_position1{ width / 2 - 5, height / 2 };
     const string game_over1 = "Game Over";
-    const Point game_over_point_position2{ weight / 2 - 9, hight/2 + 2};
+    const Point game_over_point_position2{ width / 2 - 9, height /2 + 2};
     const string game_over2 = "Press Enter to exit";
     const int snake_size_for_win = 10;
-    const Point win_point_position = { weight / 2 - 15, hight / 2 };
+    const Point win_point_position = { width / 2 - 15, height / 2 };
     const string win = "Congratulations! This game is yours";
+	const int count_of_block = 50;
+	vector<Point> block;
+	const char block_symbol = 186;
 
     Snake snake;
     Point food;
     PrintHelper pr;
 
 
-    Point change(Point p, char c);
-    Point generate_food_position();
-    void draw_a_field();
-    char generate_new_direction();
-    void threadFunction1(char& new_dir, bool& you_win);
-    void threadFunction2(char& new_dir, bool& cond);
-    void clear();
+    Point Change(Point p, char c);
+	Point GeneratePoint();
+    Point GenerateFoodPosition();
+	void GenerateBlockPosition();
+	bool PointBelongsToTheBlock(Point p);
+    void DrawTheField();
+    char GenerateNewDirection();
+	void move_block(Point& p, int i);
+    void ThreadFunction1(char& new_dir, bool& you_win);
+    void ThreadFunction2(char& new_dir, bool& cond);
+    void Clear();
 public:
     Logic();
     void Run();
